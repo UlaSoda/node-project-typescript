2c36742d55d3ca9797947ef2b3766e9a64243173
Ula <ula.soda@gmail.com>
2024-05-31 15:06:51 +0800

    新增swagger註解未完成  新增ejs畫面

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
+import path from 'path';
 import helloRoutes from "./routes/helloRoutes";
 import productRoutes from "./routes/productRoutes";
 // import { setupSwagger } from './setupSwagger';
 import sequelize from "./config/database";
 import dotenv from "dotenv";
-
+import axios from 'axios';
 import swaggerUi from "swagger-ui-express";
 const swaggerDocument = require("../swagger-output.json");
-
+const engine = require('ejs-mate');
 // 指定 .env 文件的路径
 dotenv.config({ path: "./config/.env" });
 
@@ -18,8 +19,31 @@ console.log("DB_SERVER:", process.env.DB_SERVER);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
+
 // 配置 express.json() 中間件以解析 JSON 請求體
 app.use(express.json());
+// 定义路由
+
+// 设置静态文件目录（如果需要）
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 定义根路由
+app.get('/', async (req, res) => {
+  try {
+    // 调用 API 获取列表数据
+    const response = await axios.get('http://localhost:3000/api/products');
+    const listData = response.data;
+
+    // 渲染 index.ejs 页面，并传递列表数据
+    res.render('index', { title: 'Express App', listData });
+  } catch (error) {
+    console.error('Error fetching list data:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 // 使用 hello 路由
 app.use("/api/hello", helloRoutes);
@@ -28,11 +52,11 @@ app.use("/api/products", productRoutes);
 // 设置 Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// 重定向根路径到 /api-docs
-app.get("/", (req, res) => {
-  // #swagger.ignore = true
-  res.redirect("/api-docs");
-});
+// // 重定向根路径到 /api-docs
+// app.get("/", (req, res) => {
+//   // #swagger.ignore = true
+//   res.redirect("/api-docs");
+// });
 
 // 同步数据库
 sequelize
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -5,21 +5,21 @@ const router = Router();
 
 // 注意修改路由顺序
 
-
 router.get(
-      "/productspages",
-      /* 	#swagger.tags = ['Product']
-            #swagger.description = '分頁顯示Products' */
-      /*    #swagger.responses[200] = { 
-            schema: [
-                {
-                    "id": 18,
-                    "name": "Product 2",
-                    "description": "Description for Product 2",
-                    "price": 20.99
-                }
-            ]} */
-      productController.getAllProductsPages
+  "/productspages",
+  /* 	#swagger.tags = ['Product']
+      #swagger.description = '分頁顯示Products'
+      #swagger.responses[200] = { 
+      schema: [
+            {
+                  "id": 18,
+                  "name": "Product 2",
+                  "description": "Description for Product 2",
+                  "price": 20.99
+            }
+      ]} 
+   */
+  productController.getAllProductsPages
 );
 
 router.get(
@@ -31,44 +31,46 @@ router.get(
 router.get(
   "/",
   /* 	#swagger.tags = ['Product']
-        #swagger.description = '取得所有Product' */
-  /*    #swagger.responses[200] = { 
-        schema: [
-            {
-                "id": 18,
-                "name": "Product 2",
-                "description": "Description for Product 2",
-                "price": 20.99
-            }
-        ]} */
+      #swagger.description = '取得所有Product'
+      #swagger.responses[200] = { 
+      schema: [
+      {
+            "id": 18,
+            "name": "Product 2",
+            "description": "Description for Product 2",
+            "price": 20.99
+      }
+      ]} 
+  */
   productController.getAllProducts
 );
 
 router.post(
   "/",
   /* 	#swagger.tags = ['Product']
-        #swagger.description = '新增Product'
-        #swagger.auto = true */
-  /*    #swagger.parameters['body'] = {
-            in: 'body',
-            description: 'Some description...',
-            required: true,
-            schema: {
-                $name: 'John Doe',
-                $description: 'Description for Product 2',
-                $price: 10
-            }
-    } */
-  /*    #swagger.responses[200] = { 
-        schema: 
-        {
-            "id": 18,
-            "name": "Product 2",
-            "description": "Description for Product 2",
-            "price": 20.99
-        }
-        ,
-        description: "User registered successfully." } */
+      #swagger.description = '新增Product'
+      #swagger.auto = true 
+      #swagger.parameters['body'] = {
+          in: 'body',
+          description: 'Some description...',
+          required: true,
+          schema: {
+              $name: 'John Doe',
+              $description: 'Description for Product 2',
+              $price: 10
+          }
+      }
+      #swagger.responses[200] = { 
+      schema: 
+      {
+          "id": 18,
+          "name": "Product 2",
+          "description": "Description for Product 2",
+          "price": 20.99
+      }
+      ,
+      description: "User registered successfully." } 
+      */
   productController.createProduct
 );
 router.put(
